ceab08e910ab2b8474480c508b18cb7cc955f02c
h-r-wells2 <hrwells10246@gmail.com>
2023-07-15 01:07:17 +0530

    Main code

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='Flex bg-slate-600 p-2 text-white py-3'>
+            <span className='ml-4 text-xl font-bold'>Social Parivartan</span>
+            <span className='ml-4 text-xl font-bold'>Social Parivartan</span>
+            <span className='ml-4 text-xl font-bold'>Social Parivartan</span>
+            <span className='ml-4 text-xl font-bold'>Social Parivartan</span>
+            <span className='ml-4 text-xl font-bold'>Social Parivartan</span>
+        </div>
+    )
+}
+
+export default Navbar
